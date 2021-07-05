230781e2dc8c6e1af84d6c2d0f9a3bd83d74e577
USER <USER@DESKTOP-GUL41FM>
2021-07-05 09:21:38 +0300

    Добавьте файлы проекта.

---
+++ PM 02 _ Vorobev 31P/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM_02___Vorobev_31P
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
